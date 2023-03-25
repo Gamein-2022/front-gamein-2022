0003b4f41a39f0619c980269a086342d3a9551df
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2023-03-25 16:09:36 +0330

    feat: add final products trade

--- src/components/TradeTable/components/TradeFinal/index.jsx
+++ src/components/TradeTable/components/TradeFinal/index.jsx
@@ -1,8 +1,46 @@
 import React from "react";
+import ErrorOutlineOutlinedIcon from "@mui/icons-material/ErrorOutlineOutlined";
+import BasicInput from "../../../BasicInput";
+import Button from "../../../Button";
+import coinImg from "../../../../assets/coin.svg";
+import sampleImg from "../../../../assets/img/mapPreview.png";
 import "./style.scss";
 
 function TradeFinal() {
-  return <div>TradeFinal</div>;
+  return (
+    <div className="trade-final">
+      <img src={sampleImg} alt="trade final" />
+      <p>
+        اینجا می‌تونی محضولات نهایی رو به بازار جهانی عرضه کنی. برای این کار،
+        محصول، تعداد و قیمت مورد نظرت رو مشخص کن. با توجه به تقاضای بازار بهت
+        گفته میشه که می‌تونی با این قیمت و تعداد بفروشی یا نه :)
+      </p>
+      <div>محصول</div>
+      <select className="trade-filter__select">
+        <option disabled selected>
+          انتخاب کن
+        </option>
+        <option>copper</option>
+        <option>cobalt</option>
+      </select>
+      <BasicInput
+        label={"قیمت پیشنهادی"}
+        placeholder="مثلا ۱۰۰۰"
+        type="number"
+      />
+      <BasicInput label={"تعداد"} placeholder="مثلا ۵۰۰" type="number" />
+      <div>موجودی انبار: {1000} عدد</div>
+      <div>
+        <img src={coinImg} alt="coin" />
+        درآمد حاصل از فروش: {123456}
+      </div>
+      <div>
+        <ErrorOutlineOutlinedIcon />
+        متن خطای مربوط به جور نبودن تعداد با تقاضای بازار D:
+      </div>
+      <Button>فروش به بازار</Button>
+    </div>
+  );
 }
 
 export default TradeFinal;
