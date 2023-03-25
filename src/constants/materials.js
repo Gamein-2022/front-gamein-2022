f6568d4c37b9d21f760e12c52dd80951c2d10272
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2023-03-25 14:12:45 +0330

    feat: add material assets

---
+++ src/constants/materials.js
@@ -0,0 +1,303 @@
+// raw materials
+import aluminium from "../assets/materials/level_1/aluminium.svg";
+import chips from "../assets/materials/level_1/chips.svg";
+import cobalt from "../assets/materials/level_1/cobalt.svg";
+import copper from "../assets/materials/level_1/copper.svg";
+import glass from "../assets/materials/level_1/glass.svg";
+import lithium from "../assets/materials/level_1/lithium.svg";
+import microphone from "../assets/materials/level_1/microphone.svg";
+import plastic from "../assets/materials/level_1/plastic.svg";
+import ports from "../assets/materials/level_1/ports.svg";
+import processors from "../assets/materials/level_1/processors.svg";
+import silicon from "../assets/materials/level_1/silicon.svg";
+import speaker from "../assets/materials/level_1/speaker.svg";
+import vibrationMotor from "../assets/materials/level_1/vibration_motor.svg";
+
+// intermediate materials level one
+import advancedDisplay from "../assets/materials/level_2/advanced_display.svg";
+import camera from "../assets/materials/level_2/camera.svg";
+import cpu from "../assets/materials/level_2/cpu.svg";
+import displayWithKeypad from "../assets/materials/level_2/display_with_keypad.svg";
+import fitnessTrackers from "../assets/materials/level_2/fitness_trackers.svg";
+import foldableDisplay from "../assets/materials/level_2/foldable_display.svg";
+import gpu from "../assets/materials/level_2/gpu.svg";
+import healthMonitors from "../assets/materials/level_2/health_monitors.svg";
+import modem2g from "../assets/materials/level_2/modem_2g.svg";
+import modem3g from "../assets/materials/level_2/modem_3g.svg";
+import modem4g from "../assets/materials/level_2/modem_4g.svg";
+import modem5g from "../assets/materials/level_2/modem_5g.svg";
+import multyCamera from "../assets/materials/level_2/multy_camera.svg";
+import phoneBattery from "../assets/materials/level_2/phone_battery.svg";
+import phonePhrame from "../assets/materials/level_2/phone_phrame.svg";
+import phonePhrameKeypad from "../assets/materials/level_2/phone_frame_keypad.svg";
+import ramAndStorage from "../assets/materials/level_2/ram_and_storage.svg";
+import storage from "../assets/materials/level_2/storage.svg";
+import tabletBattery from "../assets/materials/level_2/tablet_battery.svg";
+import tabletFrame from "../assets/materials/level_2/tablet_frame.svg";
+import touchDisplay from "../assets/materials/level_2/touch_display.svg";
+import watchBand from "../assets/materials/level_2/watch_band.svg";
+import watchBattery from "../assets/materials/level_2/watch_battery.svg";
+import watchDisplay from "../assets/materials/level_2/watch_display.svg";
+import watchFrame from "../assets/materials/level_2/watch_frame.svg";
+
+// intermediate materials level two
+import communication2g from "../assets/materials/level_3/communication_2g.svg";
+import communication3g from "../assets/materials/level_3/communication_3g.svg";
+import communication4g from "../assets/materials/level_3/communication_4g.svg";
+import communication5g from "../assets/materials/level_3/communication_5g.svg";
+import mediaMultyCamera from "../assets/materials/level_3/media_multy_camera.svg";
+import mediaNoCamera from "../assets/materials/level_3/media_no_camera.svg";
+import mediaSingleCamera from "../assets/materials/level_3/media_single_camera.svg";
+import phoneBody1 from "../assets/materials/level_3/phone_body_1.svg";
+import phoneBody2 from "../assets/materials/level_3/phone_body_2.svg";
+import phoneBody3 from "../assets/materials/level_3/phone_body_3.svg";
+import phoneBody4 from "../assets/materials/level_3/phone_body_4.svg";
+import quantumComputation from "../assets/materials/level_3/quantum_computation.svg";
+import socAndMemory from "../assets/materials/level_3/soc_and_memory.svg";
+import tabletBody1 from "../assets/materials/level_3/tablet_body_1.svg";
+import tabletBody2 from "../assets/materials/level_3/tablet_body_2.svg";
+import watchExternal from "../assets/materials/level_3/watch_external.svg";
+import watchInternal from "../assets/materials/level_3/watch_internal.svg";
+import watchSensors from "../assets/materials/level_3/watch_sensors.svg";
+
+// final materials
+
+export const RAW_MATERIALS = [
+  {
+    name: "aluminium",
+    icon: aluminium,
+  },
+  {
+    name: "chips",
+    icon: chips,
+  },
+  {
+    name: "cobalt",
+    icon: cobalt,
+  },
+  {
+    name: "copper",
+    icon: copper,
+  },
+  {
+    name: "glass",
+    icon: glass,
+  },
+  {
+    name: "lithium",
+    icon: lithium,
+  },
+  {
+    name: "microphone",
+    icon: microphone,
+  },
+  {
+    name: "plastic",
+    icon: plastic,
+  },
+  {
+    name: "ports",
+    icon: ports,
+  },
+  {
+    name: "processors",
+    icon: processors,
+  },
+  {
+    name: "silicon",
+    icon: silicon,
+  },
+  {
+    name: "speaker",
+    icon: speaker,
+  },
+  {
+    name: "vibrationMotor",
+    icon: vibrationMotor,
+  },
+];
+
+export const INTERMEDIATE_MATERIALS_LEVEL_ONE = [
+  {
+    name: "advancedDisplay",
+    icon: advancedDisplay,
+  },
+  {
+    name: "camera",
+    icon: camera,
+  },
+  {
+    name: "cpu",
+    icon: cpu,
+  },
+  {
+    name: "displayWithKeypad",
+    icon: displayWithKeypad,
+  },
+  {
+    name: "fitnessTrackers",
+    icon: fitnessTrackers,
+  },
+  {
+    name: "foldableDisplay",
+    icon: foldableDisplay,
+  },
+  {
+    name: "gpu",
+    icon: gpu,
+  },
+  {
+    name: "healthMonitors",
+    icon: healthMonitors,
+  },
+  {
+    name: "modem2g",
+    icon: modem2g,
+  },
+  {
+    name: "modem3g",
+    icon: modem3g,
+  },
+  {
+    name: "modem4g",
+    icon: modem4g,
+  },
+  {
+    name: "modem5g",
+    icon: modem5g,
+  },
+  {
+    name: "multyCamera",
+    icon: multyCamera,
+  },
+  {
+    name: "phoneBattery",
+    icon: phoneBattery,
+  },
+  {
+    name: "phonePhrame",
+    icon: phonePhrame,
+  },
+  {
+    name: "phonePhrameKeypad",
+    icon: phonePhrameKeypad,
+  },
+  {
+    name: "ramAndStorage",
+    icon: ramAndStorage,
+  },
+  {
+    name: "storage",
+    icon: storage,
+  },
+  {
+    name: "tabletBattery",
+    icon: tabletBattery,
+  },
+  {
+    name: "tabletFrame",
+    icon: tabletFrame,
+  },
+  {
+    name: "touchDisplay",
+    icon: touchDisplay,
+  },
+  {
+    name: "watchBand",
+    icon: watchBand,
+  },
+  {
+    name: "watchBattery",
+    icon: watchBattery,
+  },
+  {
+    name: "watchDisplay",
+    icon: watchDisplay,
+  },
+  {
+    name: "watchFrame",
+    icon: watchFrame,
+  },
+];
+
+export const INTERMEDIATE_MATERIALS_LEVEL_TWO = [
+  {
+    name: "communication2g",
+    icon: communication2g,
+  },
+  {
+    name: "communication3g",
+    icon: communication3g,
+  },
+  {
+    name: "communication4g",
+    icon: communication4g,
+  },
+  {
+    name: "communication5g",
+    icon: communication5g,
+  },
+  {
+    name: "mediaMultyCamera",
+    icon: mediaMultyCamera,
+  },
+  {
+    name: "mediaNoCamera",
+    icon: mediaNoCamera,
+  },
+  {
+    name: "mediaSingleCamera",
+    icon: mediaSingleCamera,
+  },
+  {
+    name: "phoneBody1",
+    icon: phoneBody1,
+  },
+  {
+    name: "phoneBody2",
+    icon: phoneBody2,
+  },
+  {
+    name: "phoneBody3",
+    icon: phoneBody3,
+  },
+  {
+    name: "phoneBody4",
+    icon: phoneBody4,
+  },
+  {
+    name: "quantumComputation",
+    icon: quantumComputation,
+  },
+  {
+    name: "socAndMemory",
+    icon: socAndMemory,
+  },
+  {
+    name: "tabletBody1",
+    icon: tabletBody1,
+  },
+  {
+    name: "tabletBody2",
+    icon: tabletBody2,
+  },
+  {
+    name: "watchExternal",
+    icon: watchExternal,
+  },
+  {
+    name: "watchInternal",
+    icon: watchInternal,
+  },
+  {
+    name: "watchSensors",
+    icon: watchSensors,
+  },
+];
+
+export const FINAL_MATERIALS = [
+  {
+    name: "",
+    icon: "",
+  },
+];
