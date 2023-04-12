e5697e305ebfc6631f4ddcb517a9763cb3db0c29
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2023-04-12 22:19:50 +0330

    feat: change table header sizes

---
+++ src/components/MiddleTable/components/Announcments/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+function Announcments() {
+  return <div>Announcments</div>;
+}
+
+export default Announcments;
--- src/components/MiddleTable/index.jsx
+++ src/components/MiddleTable/index.jsx
@@ -6,9 +6,11 @@ import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
 
 import newsLogo from "../../assets/news.svg";
 import announcementLogo from "../../assets/announcement.svg";
+import News from "./components/News";
+import Announcments from "./components/Announcments";
 
 function MiddleTable() {
-  const [activeTab, setActiveTab] = useState("storage");
+  const [activeTab, setActiveTab] = useState("news");
   const [open, setOpen] = useState(false);
 
   return (
@@ -21,26 +23,30 @@ function MiddleTable() {
       <div className="middle-table__header" onClick={() => setOpen(true)}>
         <div
           className={classNames("middle-table__header-item", {
-            "middle-table__header-item--active": activeTab === "production",
+            "middle-table__header-item--active": activeTab === "news",
           })}
-          onClick={() => setActiveTab("production")}
+          onClick={() => setActiveTab("news")}
         >
-          <img className="middle-table__logo" src={newsLogo} alt="production" />
+          <img className="middle-table__logo" src={newsLogo} alt="news" />
           اخبار
         </div>
         <div
           className={classNames("middle-table__header-item", {
-            "middle-table__header-item--active": activeTab === "storage",
+            "middle-table__header-item--active": activeTab === "announcments",
           })}
-          onClick={() => setActiveTab("storage")}
+          onClick={() => setActiveTab("announcments")}
         >
-          <img className="middle-table__logo" src={announcementLogo} alt="storage" />
+          <img
+            className="middle-table__logo"
+            src={announcementLogo}
+            alt="announcments"
+          />
           اطلاعیه‌ها
         </div>
       </div>
       <div className="middle-table__body">
-        {activeTab === "storage" && <div></div>}
-        {activeTab === "production" && <div></div>}
+        {activeTab === "news" && <News />}
+        {activeTab === "announcments" && <Announcments />}
       </div>
       <div className="middle-table__close-icon" onClick={() => setOpen(false)}>
         <ExpandCircleDownIcon fontSize="large" />
