1fc760b6cebf8ae4014fa4e42e10df37fa6e1ee4
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2023-04-28 13:54:23 +0330

    feat: add game doc

--- src/pages/Support/components/GameGuide/index.jsx
+++ src/pages/Support/components/GameGuide/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import doc from "../../../../assets/guide/doc.pdf";
+
 import "./style.scss";
 
 function GameGuide() {
-  return <div></div>;
+  return (
+    <div className="game-guide">
+      <iframe src={doc} frameborder="0"></iframe>
+    </div>
+  );
 }
 
 export default GameGuide;
