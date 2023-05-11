df81bb2a3f6bffc8d3d2d1feeab119e57d023744
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2023-05-11 10:46:25 +0330

    fix: recycle

--- src/components/LeftTable/components/Recycle/index.jsx
+++ src/components/LeftTable/components/Recycle/index.jsx
@@ -16,198 +16,210 @@ import { formatPrice } from "../../../../utils/formatters";
 import Modal from "../../../Modal";
 
 function Recycle({ updateBuildings }) {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
-  const [upgradeBuildingModalOpen, setUpgradeBuildingModalOpen] =
-    useState(false);
-  const [deleteBuildingModalOpen, setDeleteBuildingModalOpen] = useState(false);
-  const [actionLoading, setActionLoading] = useState(false);
-  const [pageError, setpageError] = useState(false);
-
-  useEffect(() => {
-    getGroundInfo(0)
-      .then((res) => res.data)
-      .then((data) => {
-        setData(data?.result);
-      })
-      .catch((error) => {
-        setpageError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  const updateGroundInfo = () => {
-    setLoading(true);
-    getGroundInfo(0)
-      .then((res) => res.data)
-      .then((data) => {
-        setData(data?.result);
-      })
-      .catch((error) => console.log(error))
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
-  const handleUpgradeBuilding = () => {
-    setActionLoading(true);
-    upgradeBuilding(data?.building?.id)
-      .then((res) => res.data)
-      .then((data) => {
-        toast.success("ساختمان با موفقیت ارتقا یافت.");
-        updateGroundInfo();
-        setUpgradeBuildingModalOpen(false);
-        updateBuildings();
-      })
-      .catch((error) => {
-        toast.error(
-          error?.response?.data?.message || "مشکلی در سامانه رخ داده‌است."
-        );
-      })
-      .finally(() => {
-        setActionLoading(false);
-      });
-  };
-
-  const handleDeleteBuilding = () => {
-    setActionLoading(true);
-    deleteBuilding(0)
-      .then((res) => res.data)
-      .then((data) => {
-        toast.success("ساختمان با موفقیت حذف شد.");
-        updateGroundInfo();
-        updateBuildings();
-        setDeleteBuildingModalOpen(false);
-      })
-      .catch((error) => {
-        toast.error(
-          error?.response?.data?.message || "مشکلی در سامانه رخ داده‌است."
-        );
-      })
-      .finally(() => {
-        setActionLoading(false);
-      });
-  };
-
   return (
-    <div className="ground0">
-      {pageError && <div className="page-error">یه مشکلی پیش اومده!</div>}
-      {!pageError && (
-        <>
-          {loading && <GameinLoading size={32} />}
-          {!loading && (
-            <>
-              {!data?.building && (
-                <>
-                  <ShopBuildings
-                    showUpgradeBuilding={false}
-                    buildings={[
-                      {
-                        name: "سوله بازیافت",
-                        type: "RECYCLE_FACTORY",
-                        img: recycleHallImg,
-                        description: "",
-                        price: data?.recycleBuildCost,
-                      },
-                    ]}
-                    ground={0}
-                    updateBuildings={updateBuildings}
-                    updateGroundInfo={updateGroundInfo}
-                  />
-                </>
-              )}
-              {data?.building?.lines
-                .filter((item) => item.status === "IN_PROGRESS")
-                .map((line) => (
-                  <Line {...line} updateLines={updateGroundInfo} />
-                ))}
-              {data?.building?.lines
-                .filter((item) => item.status === "OFF")
-                .map((line) => (
-                  <Line {...line} updateLines={updateGroundInfo} />
-                ))}
-              {data?.building?.lines
-                .filter((item) => item.status === "NOT_INITIAL")
-                .map((line) => (
-                  <Line {...line} updateLines={updateGroundInfo} />
-                ))}
-              {data?.building && (
-                <div className="shop-buildings__btns">
-                  {data?.building && !data?.building?.isUpgraded && (
-                    <Button
-                      onClick={() => setUpgradeBuildingModalOpen(true)}
-                      className="shop-buildings__upgrade-btn"
-                    >
-                      ارتقای ساختمان
-                    </Button>
-                  )}
-                  {data?.building && (
-                    <Button
-                      type="error"
-                      onClick={() => setDeleteBuildingModalOpen(true)}
-                      className="shop-buildings__upgrade-btn"
-                    >
-                      حذف ساختمان
-                    </Button>
-                  )}
-                </div>
-              )}
-            </>
-          )}
-          <Modal
-            open={upgradeBuildingModalOpen}
-            onClose={() => setUpgradeBuildingModalOpen(false)}
-          >
-            <div>آیا مطمئن هستید می‌خواهید ساختمان را ارتقا دهید؟</div>
-            <div>
-              هزینه ارتقا ساختمان: {formatPrice(data?.building?.upgradeCost)}{" "}
-              جی‌کوین
-            </div>
-            <div className="extend-ground__btns">
-              <Button
-                className="extend-ground__btn-yes"
-                onClick={handleUpgradeBuilding}
-                disabled={actionLoading}
-              >
-                بله
-              </Button>
-              <Button
-                onClick={() => setUpgradeBuildingModalOpen(false)}
-                type="error"
-                disabled={actionLoading}
-              >
-                بازگشت
-              </Button>
-            </div>
-          </Modal>
-          <Modal
-            open={deleteBuildingModalOpen}
-            onClose={() => setDeleteBuildingModalOpen(false)}
-          >
-            <div>آیا مطمئن هستید می‌خواهید ساختمان را حذف کنید؟</div>
-            <div className="extend-ground__btns">
-              <Button
-                className="extend-ground__btn-yes"
-                onClick={handleDeleteBuilding}
-                disabled={actionLoading}
-              >
-                بله
-              </Button>
-              <Button
-                onClick={() => setDeleteBuildingModalOpen(false)}
-                type="error"
-                disabled={actionLoading}
-              >
-                بازگشت
-              </Button>
-            </div>
-          </Modal>
-        </>
-      )}
+    <div
+      style={{
+        textAlign: "center",
+        margin: 16,
+        direction: "ltr",
+        fontWeight: 600,
+      }}
+    >
+      comming soon...
     </div>
   );
+  // const [data, setData] = useState();
+  // const [loading, setLoading] = useState(true);
+  // const [upgradeBuildingModalOpen, setUpgradeBuildingModalOpen] =
+  //   useState(false);
+  // const [deleteBuildingModalOpen, setDeleteBuildingModalOpen] = useState(false);
+  // const [actionLoading, setActionLoading] = useState(false);
+  // const [pageError, setpageError] = useState(false);
+
+  // useEffect(() => {
+  //   getGroundInfo(0)
+  //     .then((res) => res.data)
+  //     .then((data) => {
+  //       setData(data?.result);
+  //     })
+  //     .catch((error) => {
+  //       setpageError(true);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  // const updateGroundInfo = () => {
+  //   setLoading(true);
+  //   getGroundInfo(0)
+  //     .then((res) => res.data)
+  //     .then((data) => {
+  //       setData(data?.result);
+  //     })
+  //     .catch((error) => console.log(error))
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // };
+
+  // const handleUpgradeBuilding = () => {
+  //   setActionLoading(true);
+  //   upgradeBuilding(data?.building?.id)
+  //     .then((res) => res.data)
+  //     .then((data) => {
+  //       toast.success("ساختمان با موفقیت ارتقا یافت.");
+  //       updateGroundInfo();
+  //       setUpgradeBuildingModalOpen(false);
+  //       updateBuildings();
+  //     })
+  //     .catch((error) => {
+  //       toast.error(
+  //         error?.response?.data?.message || "مشکلی در سامانه رخ داده‌است."
+  //       );
+  //     })
+  //     .finally(() => {
+  //       setActionLoading(false);
+  //     });
+  // };
+
+  // const handleDeleteBuilding = () => {
+  //   setActionLoading(true);
+  //   deleteBuilding(0)
+  //     .then((res) => res.data)
+  //     .then((data) => {
+  //       toast.success("ساختمان با موفقیت حذف شد.");
+  //       updateGroundInfo();
+  //       updateBuildings();
+  //       setDeleteBuildingModalOpen(false);
+  //     })
+  //     .catch((error) => {
+  //       toast.error(
+  //         error?.response?.data?.message || "مشکلی در سامانه رخ داده‌است."
+  //       );
+  //     })
+  //     .finally(() => {
+  //       setActionLoading(false);
+  //     });
+  // };
+
+  // return (
+  //   <div className="ground0">
+  //     {pageError && <div className="page-error">یه مشکلی پیش اومده!</div>}
+  //     {!pageError && (
+  //       <>
+  //         {loading && <GameinLoading size={32} />}
+  //         {!loading && (
+  //           <>
+  //             {!data?.building && (
+  //               <>
+  //                 <ShopBuildings
+  //                   showUpgradeBuilding={false}
+  //                   buildings={[
+  //                     {
+  //                       name: "سوله بازیافت",
+  //                       type: "RECYCLE_FACTORY",
+  //                       img: recycleHallImg,
+  //                       description: "",
+  //                       price: data?.recycleBuildCost,
+  //                     },
+  //                   ]}
+  //                   ground={0}
+  //                   updateBuildings={updateBuildings}
+  //                   updateGroundInfo={updateGroundInfo}
+  //                 />
+  //               </>
+  //             )}
+  //             {data?.building?.lines
+  //               .filter((item) => item.status === "IN_PROGRESS")
+  //               .map((line) => (
+  //                 <Line {...line} updateLines={updateGroundInfo} />
+  //               ))}
+  //             {data?.building?.lines
+  //               .filter((item) => item.status === "OFF")
+  //               .map((line) => (
+  //                 <Line {...line} updateLines={updateGroundInfo} />
+  //               ))}
+  //             {data?.building?.lines
+  //               .filter((item) => item.status === "NOT_INITIAL")
+  //               .map((line) => (
+  //                 <Line {...line} updateLines={updateGroundInfo} />
+  //               ))}
+  //             {data?.building && (
+  //               <div className="shop-buildings__btns">
+  //                 {data?.building && !data?.building?.isUpgraded && (
+  //                   <Button
+  //                     onClick={() => setUpgradeBuildingModalOpen(true)}
+  //                     className="shop-buildings__upgrade-btn"
+  //                   >
+  //                     ارتقای ساختمان
+  //                   </Button>
+  //                 )}
+  //                 {data?.building && (
+  //                   <Button
+  //                     type="error"
+  //                     onClick={() => setDeleteBuildingModalOpen(true)}
+  //                     className="shop-buildings__upgrade-btn"
+  //                   >
+  //                     حذف ساختمان
+  //                   </Button>
+  //                 )}
+  //               </div>
+  //             )}
+  //           </>
+  //         )}
+  //         <Modal
+  //           open={upgradeBuildingModalOpen}
+  //           onClose={() => setUpgradeBuildingModalOpen(false)}
+  //         >
+  //           <div>آیا مطمئن هستید می‌خواهید ساختمان را ارتقا دهید؟</div>
+  //           <div>
+  //             هزینه ارتقا ساختمان: {formatPrice(data?.building?.upgradeCost)}{" "}
+  //             جی‌کوین
+  //           </div>
+  //           <div className="extend-ground__btns">
+  //             <Button
+  //               className="extend-ground__btn-yes"
+  //               onClick={handleUpgradeBuilding}
+  //               disabled={actionLoading}
+  //             >
+  //               بله
+  //             </Button>
+  //             <Button
+  //               onClick={() => setUpgradeBuildingModalOpen(false)}
+  //               type="error"
+  //               disabled={actionLoading}
+  //             >
+  //               بازگشت
+  //             </Button>
+  //           </div>
+  //         </Modal>
+  //         <Modal
+  //           open={deleteBuildingModalOpen}
+  //           onClose={() => setDeleteBuildingModalOpen(false)}
+  //         >
+  //           <div>آیا مطمئن هستید می‌خواهید ساختمان را حذف کنید؟</div>
+  //           <div className="extend-ground__btns">
+  //             <Button
+  //               className="extend-ground__btn-yes"
+  //               onClick={handleDeleteBuilding}
+  //               disabled={actionLoading}
+  //             >
+  //               بله
+  //             </Button>
+  //             <Button
+  //               onClick={() => setDeleteBuildingModalOpen(false)}
+  //               type="error"
+  //               disabled={actionLoading}
+  //             >
+  //               بازگشت
+  //             </Button>
+  //           </div>
+  //         </Modal>
+  //       </>
+  //     )}
+  //   </div>
+  // );
 }
 
 export default Recycle;
