5b44208b8b9e71d1bc879eecf1232505a2031895
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2023-06-02 10:45:06 +0330

    feat: add guide

--- src/pages/Support/components/ProductsBOM/index.jsx
+++ src/pages/Support/components/ProductsBOM/index.jsx
@@ -1,18 +1,43 @@
 import React from "react";
-import Gphone1BOM from "../../../../assets/guide/Gphone1BOM.svg";
-import Gphone2BOM from "../../../../assets/guide/Gphone2BOM.svg";
-import Gphone3BOM from "../../../../assets/guide/Gphone3BOM.svg";
+import Gphone4 from "../../../../assets/guide/Gphone4.svg";
+import Gphone5 from "../../../../assets/guide/Gphone5.svg";
+import Gphone6 from "../../../../assets/guide/Gphone6.svg";
+import Gphone7 from "../../../../assets/guide/Gphone7.svg";
+import Gphone8 from "../../../../assets/guide/Gphone8.svg";
+import Gphone9 from "../../../../assets/guide/Gphone9.svg";
+import GphoneX from "../../../../assets/guide/GphoneX.svg";
+import Gtab1 from "../../../../assets/guide/Gtab1.svg";
+import Gtab2 from "../../../../assets/guide/Gtab2.svg";
+import Gtab3 from "../../../../assets/guide/Gtab3.svg";
+import Gwatch from "../../../../assets/guide/Gwatch.svg";
+
 import "./style.scss";
 
 function ProductsBOM() {
   return (
     <div className="products-bom">
-      <h3>Gphone1</h3>
-      <img src={Gphone1BOM} alt="" />
-      <h3>Gphone2</h3>
-      <img src={Gphone2BOM} alt="" />
-      <h3>Gphone3</h3>
-      <img src={Gphone3BOM} alt="" />
+      <h3>Gphone4</h3>
+      <img src={Gphone4} alt="" />
+      <h3>Gphone5</h3>
+      <img src={Gphone5} alt="" />
+      <h3>Gphone6</h3>
+      <img src={Gphone6} alt="" />
+      <h3>Gphone7</h3>
+      <img src={Gphone7} alt="" />
+      <h3>Gphone8</h3>
+      <img src={Gphone8} alt="" />
+      <h3>Gphone9</h3>
+      <img src={Gphone9} alt="" />
+      <h3>GphoneX</h3>
+      <img src={GphoneX} alt="" />
+      <h3>Gtab1</h3>
+      <img src={Gtab1} alt="" />
+      <h3>Gtab2</h3>
+      <img src={Gtab2} alt="" />
+      <h3>Gtab3</h3>
+      <img src={Gtab3} alt="" />
+      <h3>Gwatch</h3>
+      <img src={Gwatch} alt="" />
     </div>
   );
 }
